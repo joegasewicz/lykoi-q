99d5694d254db3aa183a9d5dd5a87625acb5a48e
joegasewicz <joegasewicz@gmail.com>
2023-11-14 21:57:48 +0000

    linked list #1

--- linked_list.go
+++ linked_list.go
@@ -1,5 +1,11 @@
 package main
 
+type LinkedList struct {
+	Size int
+	Head *Node
+	Tail *Node
+}
+
 // Node is the first node in the list
 type Node struct {
 	Next *Node
@@ -14,54 +20,54 @@ func Init() *Node {
 
 // Destroy destroys the linked list. No operations are permitted to
 // be called on the linked list after Destroy is called
-func Destroy(n *Node) error {
-	n = nil
+func Destroy(l *LinkedList) error {
+	l = nil
 	return nil
 }
 
 // InsertNext inserts a node after the current linked list element.
 // If the element is nil then the new element gets placed at the head
 // of the list.
-func (n *Node) InsertNext() error {
+func (l *LinkedList) InsertNext() error {
 	return nil
 }
 
 // RemoveNext removes the next element after element from the list.
-func (n *Node) RemoveNext(element *Node) error {
+func (l *LinkedList) RemoveNext(element *Node) error {
 	return nil
 }
 
-// Size returns the total number of nodes in the list.
-func (n *Node) Size() int {
+// ListSize returns the total number of nodes in the list.
+func (l *LinkedList) ListSize() int {
 	return 0
 }
 
-// Head returns the head of the list
-func (n *Node) Head() *Node {
-	return n
+// ListHead returns the head of the list
+func (l *LinkedList) ListHead() *Node {
+	return l.Head
 }
 
-// Tail returns the tail node of the list.
-func (n *Node) Tail() *Node {
+// ListTail returns the tail node of the list.
+func (l *LinkedList) ListTail() *Node {
 	return nil
 }
 
 // IsHead determines if element is the head of the list.
-func (n *Node) IsHead(element *Node) bool {
+func (l *LinkedList) IsHead(element *Node) bool {
 	return true
 }
 
 // IsTail determines if element is the tail of the list.
-func (n *Node) IsTail(element *Node) bool {
+func (l *LinkedList) IsTail(element *Node) bool {
 	return true
 }
 
 // Evaluate returns the data of element's Node
-func (n *Node) Evaluate(element *Node) *interface{} {
+func (l *LinkedList) Evaluate(element *Node) *interface{} {
 	return element.Data
 }
 
 // NextNode returns the next Node in the list.
-func (n *Node) NextNode(element *Node) *Node {
+func (l *LinkedList) NextNode(element *Node) *Node {
 	return element.Next
 }
