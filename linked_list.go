4eeb255bb33400153463935d943e746723577864
joepostoffice <joe.gasewicz@postoffice.co.uk>
2023-11-15 18:23:06 +0000

    work form last night

--- linked_list.go
+++ linked_list.go
@@ -24,6 +24,7 @@ func Init(data *interface{}) *LinkedList {
 	return &LinkedList{
 		Size: 1,
 		Head: &n,
+		Tail: &n,
 	}
 }
 
@@ -52,27 +53,51 @@ func (l *LinkedList) InsertNext(currNode *Node, data *interface{}) (error, *Node
 			return err, l.Head.Next
 		}
 		l.Head.Next = &newNode
+		l.Size++
+		l.Tail = &newNode
 		return nil, l.Head.Next
 	}
 	node = l.Head.Next
 	for {
 		if currNode.Data == node.Data || node.Next == nil {
 			node.Next = &newNode
-			break
+			l.Size++
+			l.Tail = &newNode
+			return nil, node.Next
 		}
 		node = node.Next
 	}
-	return nil, node
 }
 
-// RemoveNext removes the next element after element from the list.
+// RemoveNext removes the next element after node.
+// If node is nil then the head of the list is removed
 func (l *LinkedList) RemoveNext(node *Node) error {
+	currNode := l.Head
+	if node == nil {
+		l.Head = nil
+		l.Size = 0
+		l.Tail = nil
+		return nil
+	}
+	for {
+		if currNode == node {
+			if currNode.Next != nil {
+				currNode.Next = nil
+				l.Size--
+				l.Head = currNode
+				break
+			} else {
+				return errors.New("no next node")
+			}
+		}
+		currNode = currNode.Next
+	}
 	return nil
 }
 
 // ListSize returns the total number of nodes in the list.
 func (l *LinkedList) ListSize() int {
-	return 0
+	return l.Size
 }
 
 // ListHead returns the head of the list
@@ -82,25 +107,65 @@ func (l *LinkedList) ListHead() *Node {
 
 // ListTail returns the tail node of the list.
 func (l *LinkedList) ListTail() *Node {
-	return nil
+	return l.Tail
 }
 
 // IsHead determines if element is the head of the list.
 func (l *LinkedList) IsHead(node *Node) bool {
-	return true
+	if l.Head == node {
+		return true
+	}
+	return false
 }
 
 // IsTail determines if element is the tail of the list.
 func (l *LinkedList) IsTail(node *Node) bool {
-	return true
+	if l.Tail == node {
+		return true
+	}
+	return false
 }
 
 // Evaluate returns the data of element's Node
-func (l *LinkedList) Evaluate(node *Node) *interface{} {
-	return node.Data
+func (l *LinkedList) Evaluate(node *Node) (error, *interface{}) {
+	currNode := l.Head
+	if currNode == node {
+		return nil, currNode.Data
+	}
+	for {
+		if currNode == node {
+			return nil, currNode.Data
+		}
+		if currNode.Next == nil {
+			break
+		}
+		currNode = currNode.Next
+	}
+	err := errors.New("cannot return data as node does not exist in list")
+	return err, nil
 }
 
 // NextNode returns the next Node in the list.
-func (l *LinkedList) NextNode(node *Node) *Node {
-	return node.Next
+func (l *LinkedList) NextNode(node *Node) (error, *Node) {
+	currNode := l.Head
+	err := errors.New("next node does not exist in list")
+	if currNode == node {
+		if currNode.Next == nil {
+			return err, nil
+		}
+		return nil, currNode.Next
+	}
+	for {
+		if currNode == node {
+			if currNode.Next == nil {
+				return err, nil
+			}
+			return nil, currNode.Next
+		}
+		if currNode.Next == nil {
+			break
+		}
+		currNode = currNode.Next
+	}
+	return err, nil
 }
